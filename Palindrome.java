08b8aa253150cce69313fe249abe4b93857e35a4
karavanii <elay.karavani@post.runi.ac.il>
2026-01-02 11:50:18 +0200

    2ndtry

--- Palindrome.java
+++ Palindrome.java
@@ -7,12 +7,12 @@ public class Palindrome {
 	
 	/** Checks if the given string is a palindrome. */
 	public static boolean isPalindrome(String s) {
-		if (s.length() == 1) {
-			return true;
-		}
-		if (s.length() == 0) {
-			return true;
-		}
-		return false;
+    if (s.length() <= 1) {
+        return true;
+    }
+    if (s.charAt(0) != s.charAt(s.length() - 1)) {
+        return false;
+    }
+    return isPalindrome(s.substring(1, s.length() - 1));
     }
 }
