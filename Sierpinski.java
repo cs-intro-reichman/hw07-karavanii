593b03348819adb3b2dc8a9b976a1be11eb085c0
karavanii <elay.karavani@post.runi.ac.il>
2026-01-02 11:47:52 +0200

    firsttry

--- Sierpinski.java
+++ Sierpinski.java
@@ -7,12 +7,24 @@ public class Sierpinski {
 	
 	// Draws a Sierpinski triangle of depth n on the standard canvass.
 	public static void sierpinski (int n) {
-		//// Replace this comment with your code
+		double x1 = 0.0;
+		double y1 = 0.0;
+		double x2 = 1.0;
+		double y2 = 0.0;
+		double y3 = 0.866;
+		double x3 = 0.5;
+		sierpinski(n, x1, x2, x3, y1, y2, y3); 
 	}
 	
 	// Does the actual drawing, recursively.
 	private static void sierpinski(int n, double x1, double x2, double x3,
 		                                 double y1, double y2, double y3) {
-		//// Replace this comment with your code
+		if (n==0) return;
+		StdDraw.line(x1, y1, x2, y2);
+		StdDraw.line(x1, y1, x3, y3);
+		StdDraw.line(x2, y2, x3, y3);
+		sierpinski(n-1, x1, (x1+x2)/2, (x1+x3)/2, y1, y2, (y1+y3)/2);
+		sierpinski(n-1, (x1+x2)/2, x2, (x2+x3)/2, y1, y2, (y2+y3)/2);
+		sierpinski(n-1, (x1+x3)/2, (x2+x3)/2, x3, (y1+y3)/2, (y2+y3)/2, y3);
 	}
 }
